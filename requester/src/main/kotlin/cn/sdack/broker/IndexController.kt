fe5a2bcbba941b3d379f83471bd747a6945ee2d9
linux_china <libing.chen@gmail.com>
2022-08-15 22:01:52 -0700

    chore: use @RestController

--- requester/src/main/kotlin/cn/sdack/broker/IndexController.kt
+++ requester/src/main/kotlin/cn/sdack/broker/IndexController.kt
@@ -1,19 +1,25 @@
 package cn.sdack.broker
 
-import org.springframework.shell.standard.ShellComponent
-import org.springframework.shell.standard.ShellMethod
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
 
 /**
  *
  * @date 2022/8/16
  * @author sdack
  */
-@ShellComponent
+@RestController
 class IndexController {
 
-    @ShellMethod
-    fun a() {
+    @Autowired
+    lateinit var roomService: RoomService
 
+    @GetMapping("/rooms")
+    fun index(): Mono<List<String>> {
+        return roomService.getAll().collectList();
     }
 
 }
